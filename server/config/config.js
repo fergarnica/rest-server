bde154b09d5f699d1818e77f9c913911fc7abdce
Fernando <garnica.fgp@gmail.com>
2019-10-10 11:40:57 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,15 @@
 // PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BASE DE DATOS
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb://fgarnica:TAyBg51RYRphs3d1@cluster0-vkbj2.mongodb.net/cafe';
+}
+process.env.URLDB =urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('./config/config');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
 
 
 // parse application/x-www-form-urlencoded
@@ -10,42 +12,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use( require('./routes/usuario'));
 
-app.get('/usuario', function (req, res) {
-    res.json('get Usuario')
+/* mongoose.connect('mongodb://localhost:27017/cafe', (err, resp) => {
+
+    if(err) throw err;
+
+    console.log('Base de Datos ONLINE');
+}); */
+
+mongoose.connect(process.env.URLDB ,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true},
+   (err, res) => {
+    if(err){
+        console.log("ERROR AL CONECTAR");
+        throw err;
+    } 
+    console.log('BBDD Online');
 });
 
-app.post('/usuario', function (req, res) {
 
-    let body = req.body;
-
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    }else{
-        res.json({
-            usuario: body
-        });
-    }
-
-    
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-});
+/* mongoose.connect('mongodb://localhost:27017/cafe', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}); */
 
 
 app.listen(process.env.PORT, () => {
